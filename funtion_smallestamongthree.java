d666700cfb93a76fdd80b59101a9e0974b7e3b9c
Jatin2672 <jatinsingh2672@gmail.com>
2022-09-01 00:35:58 +0530

    Some pattern questions
    .....

---
+++ funtion_smallestamongthree.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class funtion_smallestamongthree {
+    public static void smallnumber(int a,int b,int c){
+        // int small=0;
+        if(a<b && a<c){
+            System.out.println("a is smallest");
+        }
+        else if(a>b && b<c){
+            System.out.println("b is smallest");
+        }else{
+            System.out.println("c is smallest");
+        }
+
+        
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        smallnumber(a, b, c);
+    }
+}
