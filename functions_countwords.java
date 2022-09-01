54119a95b41a648a005dfd7b277863a11001a122
Jatin2672 <jatinsingh2672@gmail.com>
2022-09-01 12:37:50 +0530

    Functions
    solved some questions on funtions.

---
+++ functions_countwords.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class functions_countwords {
+    // public static int words(String str){
+    //     int count=0;
+    //     return str.length();
+    // }
+    // public static void main(String args[]){
+    //     Scanner sc=new Scanner(System.in);
+    //     Sy/stem.out.print("Input the string");
+    //     String str=sc.nextLine();
+    //     System.out.println("the no of words in string is :" + words(str));
+    // }
+    public static int words(String str){
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==' '){
+                count++;
+            }
+        }
+        return count+1;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Input the string");
+        String str=sc.nextLine();
+        System.out.println("the no of words in string is :" + words(str));
+    }
+}
---
+++ functions_middlestring.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class functions_middlestring {
+    public static String midstr(String str){
+        int position;
+        int length;
+        if(str.length() % 2==0){
+            position=str.length()/2 -1;
+            length=2;
+        }
+        else{
+            position=str.length()/2;
+            length=1;
+        }
+        return str.substring(position,position+length);
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Input a string :");
+        String str=sc.nextLine();
+        System.out.println("the middle character of the string is :" + midstr(str));
+    }
+    
+}
